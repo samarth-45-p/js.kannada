2b66c2b2bc04bde1346015b2e67da1693b3c749e
samarth-45-p <samarthh9353@gmail.com>
2025-07-28 16:23:12 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3)//we are using node js not browser
+
+console.log(3+3); console.log("samarth")
+
+let name="samarth"
+let age=18
+let loggedIn=false
+
+//number=2 to power 53
+//bigInt
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefined=         
+//symbol=unique
+
+
+//object
+
+console.log(typeof null);//object
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail="samarthh9353@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+//accountId=2//not allowed
+
+/*prefer not to use var
+beacause of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
